2602b8948e1d2726e43605d96d8d3f802249f4bf
Jaykumar Halpati <rameshhalpati007@gmail.com>
2020-09-23 20:37:20 +0100

    Updates

---
+++ precache-manifest.092f5e7e1c630c79e96760290a94873d.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "8574443d0f8af8ddc76f9b7ce7760786",
+    "url": "/NASA-APOD/index.html"
+  },
+  {
+    "revision": "e77f6212aa7103b10f2a",
+    "url": "/NASA-APOD/static/css/main.882ffa39.chunk.css"
+  },
+  {
+    "revision": "d75eb4db791cc7ccf9b3",
+    "url": "/NASA-APOD/static/js/2.391df923.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/NASA-APOD/static/js/2.391df923.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "e77f6212aa7103b10f2a",
+    "url": "/NASA-APOD/static/js/main.01deb7f4.chunk.js"
+  },
+  {
+    "revision": "452a858aa7aa585f21fa",
+    "url": "/NASA-APOD/static/js/runtime-main.4a5eb6b7.js"
+  }
+]);
